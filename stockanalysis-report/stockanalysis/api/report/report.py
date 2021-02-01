e8ff3e2caa84e1e363d7b80c805812dd32dd3e59
David Rocker <david.rocker1994@gmail.com>
2021-01-31 21:31:00 -0500

    return report image for stock
    may need to re-evaluate how the image is built or saved. right now, seems to cut itself off

--- stockanalysis-report/stockanalysis/api/report/report.py
+++ stockanalysis-report/stockanalysis/api/report/report.py
@@ -1,6 +1,8 @@
 import json
-from stockanalysis.report.build_yfinance_report import build_yfinance_report
-
+import io
+import plotly
+from stockanalysis.report.build_yfinance_report import build_yfinance_report, build_yfinance_report_image
+from flask import send_file
 
 def get_yfinance_report(ticker):
     try:
@@ -21,3 +23,25 @@ def get_yfinance_report(ticker):
         return msg, 400
     return data, 200
 
+
+def get_yfinance_report_image(ticker):
+    try:
+        ticker_obj, data = build_yfinance_report(ticker.upper())
+    except ValueError:
+        msg = {
+            "details": "Invalid stock ticker: {}".format(ticker),
+            "status": 400,
+            "title": "Invalid ticker provided"
+        }
+        return msg, 400
+    except KeyError:
+        msg = {
+            "details": "Invalid stock ticker: {}".format(ticker),
+            "status": 400,
+            "title": "Invalid ticker provided"
+        }
+        return msg, 400
+
+    fig = build_yfinance_report_image(ticker_obj, data)
+    fig.write_image("report.png", scale=1.0, width=5000, height=10000)
+    return send_file("report.png", mimetype='image/png'), 200
